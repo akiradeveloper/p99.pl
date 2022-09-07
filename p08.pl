268e66d59ef0f671291b6cd173cb5bafdbcc93c8
Akira Hayakawa <ruby.wktk@gmail.com>
2022-09-07 20:40:02 +0900

    add p08 answer

--- p08.pl
+++ p08.pl
@@ -4,3 +4,8 @@ comp([X|XS], [], L) :- comp(XS, [X], L).
 comp([X|XS], [X|YS], L) :- comp(XS, [X|YS], L).
 comp([X|XS], [Y|YS], L) :- comp(XS, [X,Y|YS], L).
 
+
+ans([],[]).
+ans([X],[X]).
+ans([X,X|Xs],Zs) :- ans([X|Xs],Zs).
+ans([X,Y|Ys],[X|Zs]) :- X \= Y, ans([Y|Ys],Zs).
