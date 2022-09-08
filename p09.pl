73a844ee37d5ecc06a931183ac9399d9c7c6a040
Akira Hayakawa <ruby.wktk@gmail.com>
2022-09-08 13:41:27 +0900

    add p09 (not solved)

---
+++ p09.pl
@@ -0,0 +1,5 @@
+pack(L1, L2) :- pack(L1, [], [], L2).
+pack([], [], L, L).
+pack([X|Xs], [], Acc, Zs) :- pack(Xs, [X], Acc, Zs). 
+pack([X|Xs], [Y|Ys], Acc, Zs) :- X == Y, pack(Xs, [X,Y|Ys], Acc, Zs).
+pack([X|Xs], [Y|Ys], Acc, Zs) :- X \= Y, pack(Xs, [X], [[Y|Ys]|Acc], Zs).
